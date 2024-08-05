c973659218e7d094984d9e103154551f6fbec85c
Ayesha-Khan12 <developerayesha582@gmail.com>
2024-08-05 23:05:33 +0500

    Add files via upload

---
+++ srcipt.js
@@ -0,0 +1,12 @@
+
+
+//JavaScript var
+
+
+var Birthday ='1990';
+document.write("My Birthday Year is" +" "+Birthday +"<br/>");
+console.log(Birthday);
+
+var Data = 'Data type of my declared variable is number';
+document.write(Data);
+console.log(Data);
